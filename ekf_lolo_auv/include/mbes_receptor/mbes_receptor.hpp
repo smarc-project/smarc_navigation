3d27a10f965a1c360a3b219345512c67e6828722
Nacho <torroba@kth.se>
2017-12-19 14:44:50 +0100

    Landmark localization for EKF. Only for sss_right and assuming one landmark per reading only

--- ekf_lolo_auv/include/mbes_receptor/mbes_receptor.hpp
+++ ekf_lolo_auv/include/mbes_receptor/mbes_receptor.hpp
@@ -10,9 +10,11 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 #include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/PointStamped.h>
 
 #include <tf/transform_listener.h>
 #include <tf/tf.h>
+
 
 typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::LaserScan, sensor_msgs::LaserScan> MyTimerPolicy;
 
@@ -28,8 +30,8 @@ private:
     message_filters::Subscriber<sensor_msgs::LaserScan>* mbes_l_subs_;
     message_filters::Subscriber<sensor_msgs::LaserScan>* mbes_r_subs_;
     message_filters::Synchronizer<MyTimerPolicy>* mbes_synch_;
-
     ros::Publisher pcl_pub_;
+    ros::Publisher landmark_pub_;
 
     // Methods
     void mbesReadingsCB(const sensor_msgs::LaserScanConstPtr & mbes_l_msg, const sensor_msgs::LaserScanConstPtr &mbes_r_msg );
--- ekf_lolo_auv/src/mbes_receptor.cpp
+++ ekf_lolo_auv/src/mbes_receptor.cpp
@@ -19,45 +19,84 @@ MBESReceptor::MBESReceptor(std::string node_name, ros::NodeHandle &nh): node_nam
 
     // RVIZ pcl output for testing
     pcl_pub_ = nh_->advertise<pcl::PointCloud<pcl::PointXYZ>>(pcl_out_top, 10);
-
+    landmark_pub_ = nh_->advertise<geometry_msgs::PointStamped>("/lolo_auv/landmark_detected", 10);
 }
 
+auto print = [](const double& n) { std::cout << " " << n; };
 
 void MBESReceptor::mbesReadingsCB(const sensor_msgs::LaserScanConstPtr &mbes_l_msg,
                                   const sensor_msgs::LaserScanConstPtr &mbes_r_msg){
 
-    pcl::PointCloud<pcl::PointXYZ> cloud_in;
+    // Compute mean of the intensities
+    double mean_r = std::accumulate(mbes_r_msg->intensities.begin(), mbes_r_msg->intensities.end(), 0) / mbes_r_msg->intensities.size();
+    std::vector<double> intensities_right;
+    std::vector<double> edges_right;
+    std::vector<double> mask {-1,0,1};
 
-    // Fill in the CloudIn data
-    cloud_in.width    = 5;
-    cloud_in.height   = 1;
-    cloud_in.is_dense = false;
-    cloud_in.points.resize (cloud_in.width * cloud_in.height);
+    // Substract the mean to each value
+    std::for_each(mbes_r_msg->intensities.begin(), mbes_r_msg->intensities.end(), [&mean_r, &intensities_right](const double& n) {
+        intensities_right.push_back(n - mean_r);
+    });
+    std::cout << std::endl;
 
-    for (size_t i = 0; i < cloud_in.points.size (); ++i)
-    {
-      cloud_in.points[i].x = 10 * rand ();
-      cloud_in.points[i].y = 10 * rand ();
-      cloud_in.points[i].z = 10 * rand ();
+    // Prewitt mask for edges
+    edges_right.push_back(0);
+    for(unsigned int i=1; i<intensities_right.size() -1; i++){
+        std::vector<double>aux {intensities_right.at(i-1),
+                                intensities_right.at(i),
+                                intensities_right.at(i+1)};
+        edges_right.push_back(std::inner_product(aux.begin(), aux.end(), mask.begin(), 0));
     }
+    edges_right.push_back(0);
 
-    tf::TransformListener sss_base_listener;
+    std::vector<int> target_pose;
+    int i = 0;
+    std::for_each(edges_right.begin(), edges_right.end(), [&target_pose, &i](const double &edge_i){
+            if(edge_i > 2 || edge_i < -2){
+                target_pose.push_back(i);
+            }
+            i++;
+    });
 
-    try{
-        sss_base_listener.waitForTransform("/lolo_auv/base_link", "/lolo_auv/sonarleft_link", mbes_l_msg->header.stamp, ros::Duration(10));
-        cloud_in.header.stamp = mbes_l_msg->header.stamp.toSec();
-        cloud_in.header.frame_id = "lolo_auv/base_link";
-        pcl_pub_.publish(cloud_in);
-        ROS_INFO("PointCloud published");
+    if(!target_pose.empty()){
+//        std::cout << "Possible poses: " << std::endl;
+//        std::for_each(target_pose.begin(), target_pose.end(), print);
+//        std::cout << std::endl;
+
+        int reminder = target_pose.size()%2;
+        int landmark_idx = (reminder == 0)? target_pose.at((target_pose.size()/2)): target_pose.at(((target_pose.size()+1)/2));
+
+        double alpha = mbes_r_msg->angle_min + mbes_r_msg->angle_increment * landmark_idx;
+        std::cout << "alpha: " << alpha << std::endl;
+
+        geometry_msgs::PointStamped landmark_msg;
+        landmark_msg.header.stamp = mbes_r_msg->header.stamp;
+        landmark_msg.header.frame_id = "lolo_auv/sonarright_link";
+        landmark_msg.point.x = mbes_r_msg->ranges.at(landmark_idx) * std::cos(alpha);
+        landmark_msg.point.y = mbes_r_msg->ranges.at(landmark_idx) * std::sin(alpha);
+        landmark_msg.point.z = 0;
+        landmark_pub_.publish(landmark_msg);
     }
-    catch(tf::TransformException &exception) {
-        ROS_ERROR("%s", exception.what());
-        ros::Duration(1.0).sleep();
-    }
-
 }
 
 
 MBESReceptor::~MBESReceptor(){
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
