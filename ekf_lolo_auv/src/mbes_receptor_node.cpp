e50b34cd513edb61444a0fe6fcab94aaafea3910
Nacho <torroba@kth.se>
2017-12-19 10:48:07 +0100

    Merged lolo_auv_nav and mbes_detector classes in same ROS node

--- ekf_lolo_auv/src/mbes_receptor_node.cpp
+++ ekf_lolo_auv/src/mbes_receptor_node.cpp
@@ -1,16 +1,16 @@
-#include "mbes_receptor/mbes_receptor.hpp"
+//#include "mbes_receptor/mbes_receptor.hpp"
 
-int main(int argc, char** argv){
+//int main(int argc, char** argv){
 
-    ros::init(argc, argv, "mbes_receptor_node");
-    ros::NodeHandle nh;
+//    ros::init(argc, argv, "mbes_receptor_node");
+//    ros::NodeHandle nh;
 
-    MBESReceptor* sss_scanner = new MBESReceptor(ros::this_node::getName(), nh);
+//    MBESReceptor* sss_scanner = new MBESReceptor(ros::this_node::getName(), nh);
 
-    ros::spin();
-    if(!ros::ok()){
-        delete sss_scanner;
-    }
+//    ros::spin();
+//    if(!ros::ok()){
+//        delete sss_scanner;
+//    }
 
-    return 0;
-}
+//    return 0;
+//}
