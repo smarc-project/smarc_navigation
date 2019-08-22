a34d96f3d27bc9eba08394ca0427a1443d83ec13
Nacho <torroba@kth.se>
2019-08-22 15:27:36 +0200

    Transfrom from press sensor to base_link now done through tf

--- sam_dead_reckoning/scripts/press_to_depth.py
+++ sam_dead_reckoning/scripts/press_to_depth.py
@@ -13,13 +13,14 @@ class Press2Depth(object):
 		self.base_frame = rospy.get_param(rospy.get_name() + '/base_frame', '/base_link')
 		self.depth_frame = rospy.get_param(rospy.get_name() + '/depth_frame', '/depth_link')
 		self.press_topic = rospy.get_param(rospy.get_name() + '/pressure_topic', '/pressure')
+		self.press_frame = rospy.get_param(rospy.get_name() + '/pressure_frame', '/pressure_link')
 		self.depth_topic = rospy.get_param(rospy.get_name() + '/depth_topic', '/depth')
 
 		self.subs = rospy.Subscriber(self.press_topic, FluidPressure, self.depthCB)
 		self.pub = rospy.Publisher(self.depth_topic, PoseWithCovarianceStamped, queue_size=10)
 
 		self.depth_msg = PoseWithCovarianceStamped()
-		self.depth_msg.header.frame_id = self.odom_frame
+		self.depth_msg.header.frame_id = self.press_frame
 		self.depth_msg.pose.covariance = [1., 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1., 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.9, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01]
 		self.depth_msg.pose.pose.orientation.w = 1.
 		
@@ -38,21 +39,21 @@ class Press2Depth(object):
 
 	def depthCB(self, press_msg):
 		try:
-			(trans,quaternion) = self.listener_odom.lookupTransform(self.base_frame, self.odom_frame, rospy.Time(0))
-			euler = tf.transformations.euler_from_quaternion(quaternion)
-			pitch = euler[1]
-			rospy.loginfo("Pitch %s", pitch)
+			# (trans,quaternion) = self.listener_odom.lookupTransform(self.base_frame, self.odom_frame, rospy.Time(0))
+			# euler = tf.transformations.euler_from_quaternion(quaternion)
+			# pitch = euler[1]
+			# rospy.loginfo("Pitch %s", pitch)
 			
-			# depth_abs is positive, must be manually negated
+			# # depth_abs is positive, must be manually negated
 			depth_abs = - self.pascal_pressure_to_depth(press_msg.fluid_pressure)
 			rospy.loginfo("Depth abs %s", depth_abs)
-			# Check signs here
-			depth_base_link = depth_abs + self.x_base_depth * np.sin(pitch)
-			rospy.loginfo("Depth base link %s", depth_base_link)
+			# # Check signs here
+			# depth_base_link = depth_abs + self.x_base_depth * np.sin(pitch)
+			# rospy.loginfo("Depth base link %s", depth_base_link)
 
 			if press_msg.fluid_pressure > 90000. and press_msg.fluid_pressure < 500000.:
 				self.depth_msg.header.stamp = rospy.Time.now()
-				self.depth_msg.pose.pose.position.z = depth_base_link # = [0., 0., 2.]
+				self.depth_msg.pose.pose.position.z = depth_abs # = [0., 0., 2.]
 				self.pub.publish(self.depth_msg)
 		
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
