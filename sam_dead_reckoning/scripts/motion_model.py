d9c49366b14eecee99ffa4733446c39a890b990b
Torroba <torroba@kth.se>
2020-01-21 09:19:32 +0100

    Motion model listeing to RPM combined feedback

--- sam_dead_reckoning/scripts/motion_model.py
+++ sam_dead_reckoning/scripts/motion_model.py
@@ -2,7 +2,7 @@
 
 import rospy
 import numpy as np
-#from sam_msgs.msg import ThrusterRPMs
+from sam_msgs.msg import ThrusterRPMs
 from geometry_msgs.msg import TwistStamped
 from uavcan_ros_bridge.msg import ESCStatus
 
@@ -11,7 +11,7 @@ class SamMM(object):
 	def __init__(self):
             self.dr_thrust_topic = rospy.get_param(rospy.get_name() + '/thrust_dr', '/motion_dr')
             self.rpm_fb_topic = rospy.get_param(rospy.get_name() + '/thrust_fb', '/rpm_fb')
-            self.subs_thrust = rospy.Subscriber(self.rpm_fb_topic, ESCStatus, self.thrustCB)	    
+            self.subs_thrust = rospy.Subscriber(self.rpm_fb_topic, ThrusterRPMs, self.thrustCB)	    
             self.control_pub = rospy.Publisher(self.dr_thrust_topic, TwistStamped, queue_size=10)
             
             self.prev_time = rospy.Time.now()
@@ -23,23 +23,23 @@ class SamMM(object):
 
             rospy.spin()
 
- 	def thrustCB(self, esc_msg):
-            if esc_msg.esc_index == 0:
-		self.rpm_0 = esc_msg.rpm
-            if esc_msg.esc_index == 1:
-		self.rpm_1 = esc_msg.rpm
+ 	def thrustCB(self, rpm_msg):
+            
+	    self.rpm_0 = rpm_msg.thruster_1_rpm
+	    self.rpm_1 = rpm_msg.thruster_2_rpm
 
             twist_msg = TwistStamped()
             twist_msg.header.stamp = rospy.Time.now()
             twist_msg.header.frame_id = "sam/base_link"
             twist_msg.twist.linear.x = (self.rpm_0 + self.rpm_1) * self.coeff
+            twist_msg.twist.linear.y = 0.0 
 #            msg_odom.twist.covariance = [0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
 #                                         0.0, 1, 0.0, 0.0, 0.0, 0.0,
 #                                         0.0, 0.0, 1, 0.0, 0.0, 0.0,
 #                                         0.0, 0.0, 0.0, 1, 0.0, 0.0,
 #                                         0.0, 0.0, 0.0, 0.0, 1, 0.0,
 #                                         0.0, 0.0, 0.0, 0.0, 0.0, 1]
-
+            
             self.control_pub.publish(twist_msg)
 
 
