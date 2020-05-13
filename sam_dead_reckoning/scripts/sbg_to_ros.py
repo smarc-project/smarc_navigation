6c990c4b24bb14cc00ac602f595abf551dacb80b
Torroba <torroba@kth.se>
2020-05-13 09:19:49 +0200

    utm->map->odom->base tree running with GPS and compass

---
+++ sam_dead_reckoning/scripts/sbg_to_ros.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import rospy
+import numpy as np
+import tf
+from geometry_msgs.msg import TwistWithCovarianceStamped
+import tf
+import message_filters
+from sbg_driver.msg import SbgImuData, SbgEkfQuat, SbgEkfEuler, SbgMag, SbgUtcTime
+from sensor_msgs.msg import Imu
+
+class SBG2ROS(object):
+
+    def sbg_cb(self, sbg_imu, sbg_quat, sbg_mag):
+        imu_msg = Imu()
+        imu_msg.header.frame_id = self.imu_frame
+        imu_msg.header.stamp = sbg_imu.header.stamp 
+        imu_msg.orientation = sbg_quat.quaternion 
+        imu_msg.angular_velocity = sbg_imu.gyro
+        imu_msg.linear_acceleration = sbg_imu.accel
+        imu_msg.linear_acceleration_covariance = [0.] * 9
+        imu_msg.linear_acceleration_covariance[0] = 100
+        imu_msg.linear_acceleration_covariance[4] = 100
+        imu_msg.linear_acceleration_covariance[8] = 100
+
+
+        self.imu_pub.publish(imu_msg)
+
+    def __init__(self):
+        self.sbg_imu_top = rospy.get_param('~sbg_imu_data', 'sbg/imu_data')
+        self.sbg_quat_top = rospy.get_param('~sbg_ekf_quat', 'sbg/ekf_quat')
+        self.sbg_mag_top = rospy.get_param('~sbg_mag', 'sbg/mag')
+        self.sbg_utc_top = rospy.get_param('~sbg_utc_time', 'sbg/utc_time')
+        self.imu_frame = rospy.get_param('~sbg_frame', 'sbg_link')
+        self.sbg_imu_out = rospy.get_param('~sbg_imu_out', 'sbg_imu')
+
+        self.imu_data_sub = message_filters.Subscriber(self.sbg_imu_top, SbgImuData)
+        self.imu_quat_sub = message_filters.Subscriber(self.sbg_quat_top, SbgEkfQuat)
+        self.imu_mag_sub = message_filters.Subscriber(self.sbg_mag_top, SbgMag)
+
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.imu_data_sub, 
+                                                               self.imu_quat_sub, self.imu_mag_sub],
+                                                              10, slop=1.0, allow_headerless=False)
+        self.ts.registerCallback(self.sbg_cb)
+
+        self.imu_pub = rospy.Publisher(self.sbg_imu_out, Imu, queue_size=10)
+
+        rospy.spin()
+
+   
+
+if __name__ == "__main__":
+    rospy.init_node('sbg_2_ros_node')
+    try:
+        pi = SBG2ROS()
+    except rospy.ROSInterruptException:
+        pass
