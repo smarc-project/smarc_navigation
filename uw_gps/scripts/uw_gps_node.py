9e5bab8b53a8e41274acc831efa6a235ddafed78
ignaciotb <ignaciotorroba@hotmail.com>
2023-04-20 13:08:45 +0200

    Driver for WL UW GPS

--- uw_gps/scripts/uw_gps_node.py
+++ uw_gps/scripts/uw_gps_node.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 """
-Create tracklog from Water Linked Underwater GPS
+Get position from Water Linked Underwater GPS
 """
-from __future__ import print_function
-import requests
 import argparse
-import time
-import logging
-import datetime
-import gpxpy
-import gpxpy.gpx
+import json
+import requests
 import rospy
-import utm 
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseStamped
+import tf
+from geodesy import utm
+import numpy as np
+from geometry_msgs.msg import Quaternion, TransformStamped
 
-class UWGps():
+class UWGPSNode():
 
     def get_data(self, url):
         try:
@@ -32,131 +29,98 @@ class UWGps():
 
         return r.json()
 
+    def get_antenna_position(self, base_url):
+        return self.get_data("{}/api/v1/config/antenna".format(base_url))
+
     def get_acoustic_position(self, base_url):
         return self.get_data("{}/api/v1/position/acoustic/filtered".format(base_url))
 
-
-    def get_global_position(self, base_url):
+    def get_global_position(self, base_url, acoustic_depth = None):
         return self.get_data("{}/api/v1/position/global".format(base_url))
-
+    
     def get_master_position(self, base_url):
         return self.get_data("{}/api/v1/position/master".format(base_url))
 
-
     def __init__(self):
-        log = logging.getLogger()
-        logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-        gps_global_pub = rospy.Publisher('/sam/dr/uw_gps_global', NavSatFix, queue_size=10)
-        odom_pub = rospy.Publisher('/sam/dr/uw_gps_odom', Odometry, queue_size=10)
-        gps_master_pub = rospy.Publisher('/sam/dr/uw_gps_master', NavSatFix, queue_size=10)
-        pose_pub = rospy.Publisher('/sam/dr/uw_gps_pose', PoseStamped, queue_size=10)
-        
-        parser = argparse.ArgumentParser(description=__doc__)
-        parser.add_argument('-u', '--url', help='Base URL to use', type=str, default='http://demo.waterlinked.com')
-        parser.add_argument('-o', '--output', help='Output filename', type=str, default='tracklog.gpx')
+        self.uw_gps_odom = rospy.get_param('~uw_gps_odom', '/sam/external/uw_gps_odom')
+        self.uw_gps_latlon = rospy.get_param('~uw_gps_latlon', '/sam/external/uw_gps_latlon')
+        self.map_frame = rospy.get_param('~map_frame', 'map')
+        self.utm_frame = rospy.get_param('~utm_frame', 'utm')
+        self.uwgps_frame = rospy.get_param('~uwgps_frame', 'sam/uwgps_link')
+        self.antenna = rospy.get_param('~bool_antenna', True)
+        self.base_url = rospy.get_param('~uwgps_server_ip', "https://demo.waterlinked.com")
 
-        args = parser.parse_args()
+        # odom_pub = rospy.Publisher('/sam/dr/uw_gps_odom', Odometry, queue_size=10)
+        # gps_master_pub = rospy.Publisher('/sam/dr/uw_gps_master', NavSatFix, queue_size=10)
+        self.gps_global_pub = rospy.Publisher(self.uw_gps_latlon, NavSatFix, queue_size=10)
+        self.uwgps_odom_pub = rospy.Publisher(self.uw_gps_odom, Odometry, queue_size=10)
 
-        base_url = args.url
-        filename = args.output
-        log.info("Creating tracklog from: {} into file {}. Press Ctrl-C to stop logging".format(base_url, filename))
+        print("Using base_url: %s" % self.base_url)
 
-        gpx = gpxpy.gpx.GPX()
 
-        # Create track
-        gpx_track = gpxpy.gpx.GPXTrack()
-        gpx.tracks.append(gpx_track)
+        while not rospy.is_shutdown():
 
-        # Create segment
-        gpx_segment_master = gpxpy.gpx.GPXTrackSegment()
-        gpx_segment_global = gpxpy.gpx.GPXTrackSegment()
+            acoustic_position = self.get_acoustic_position(self.base_url)
+            antenna_position = None
+            if self.antenna:
+                antenna_position = self.get_antenna_position(self.base_url)
+            depth = None
+            if acoustic_position:
+                if antenna_position:
+                    print(acoustic_position)
+                    print(antenna_position)
+                    print("Current acoustic position relative to antenna. X: {}, Y: {}, Z: {}".format(
+                        acoustic_position["x"] - antenna_position["x"],
+                        acoustic_position["y"] - antenna_position["y"],
+                        acoustic_position["z"] - antenna_position["depth"]))
+                else:
+                    print("Current acoustic position. X: {}, Y: {}, Z: {}".format(
+                        acoustic_position["x"],
+                        acoustic_position["y"],
+                        acoustic_position["z"]))
+                depth = acoustic_position["z"]
 
-        gpx_track.segments.append(gpx_segment_master)
-        gpx_track.segments.append(gpx_segment_global)
+            global_position = self.get_global_position(self.base_url)
+            if global_position:
+                print("Current global position. Latitude: {}, Longitude: {}, Depth: {}".format(
+                    global_position["lat"],
+                    global_position["lon"],
+                    depth))
+                
+                utm_uwgps = utm.fromLatLong(
+                    global_position["lat"], global_position["lon"])
 
-        # Open file for writing so we don't get an access denied error
-        # after a full log session is completed
-        f = open(filename, "w")
-
-        try:
-            while True:
-                pos_global = self.get_global_position(base_url)
-                if not pos_global:
-                    log.warning("Got no global position")
-                    continue
-
-                lat_global = pos_global["lat"]
-                lon_global = pos_global["lon"]
-
-                pos_master = self.get_master_position(base_url)
-                if not pos_master:
-                    log.warning("Got no master position")
-                    continue
-
-                lat_master = pos_master["lat"]
-                lon_master = pos_master["lon"]
-
-                acoustic = self.get_acoustic_position(base_url)
-                if not acoustic:
-                    log.warning("Got no acoustic position")
-                    continue
-
-                depth = acoustic["z"]
-                altitude = -depth
-
-                log.info("Global: Lat: {} Lon: {} Alt: {}".format(lat_global, lon_global, altitude))
-                gpx_segment_global.points.append(gpxpy.gpx.GPXTrackPoint(lat_global, lon_global, elevation=-altitude, time=datetime.datetime.utcnow()))
-
-                log.info("Master: Lat: {} Lon: {}".format(lat_master, lon_master))
-                gpx_segment_master.points.append(gpxpy.gpx.GPXTrackPoint(lat_master, lon_master, time=datetime.datetime.utcnow()))
+                # UW GPS 
+                t_now = rospy.Time.now()
+                rot = [0., 0., 0., 1.]
+                odom_msg = Odometry()
+                odom_msg.header.stamp = t_now
+                odom_msg.header.frame_id = self.utm_frame
+                odom_msg.child_frame_id = self.uwgps_frame
+                odom_msg.pose.covariance = [0.] * 36
+                odom_msg.pose.pose.position.x = utm_uwgps.northing
+                odom_msg.pose.pose.position.y = utm_uwgps.easting
+                odom_msg.pose.pose.position.z = depth
+                odom_msg.pose.pose.orientation = Quaternion(*rot)
+                self.uwgps_odom_pub.publish(odom_msg)
 
                 gps_msg = NavSatFix()
-                gps_msg.header.frame_id = 'world'
-                gps_msg.header.stamp = rospy.Time.now()
-                gps_msg.latitude = lat_global
-                gps_msg.longitude = lon_global
-                gps_msg.altitude = altitude 
-                gps_global_pub.publish(gps_msg)
+                gps_msg.header.frame_id = self.uwgps_frame
+                gps_msg.header.stamp = t_now
+                gps_msg.latitude = global_position["lat"]
+                gps_msg.longitude = global_position["lon"]
+                gps_msg.altitude = -depth
+                self.gps_global_pub.publish(gps_msg)
 
-                gps_msg.header.frame_id = 'uw_master'
-                gps_msg.header.stamp = rospy.Time.now()
-                gps_msg.latitude = lat_master 
-                gps_msg.longitude = lon_master 
-                gps_msg.altitude = 0.0 
-                gps_master_pub.publish(gps_msg)
+            else:
+                rospy.logwarn("UW GPS: global position not received")
 
-                pose_msg = PoseStamped()
-                pose_msg.header.frame_id = 'map'
-                pose_msg.header.stamp = rospy.Time.now()
-                pose_msg.pose.position.x = lat_global
-                pose_msg.pose.position.y = lon_global
-                pose_msg.pose.position.z = -depth 
-
-                pose_pub.publish(pose_msg)
-
-                #  (utm_x, utm_y, utm_zone, utm_letter) = utm.from_latlon(lat_global, lon_global)
-                #  odom_msg = Odometry()
-                #  odom_msg.header.frame_id = 'world'
-                #  odom_msg.child_frame_id = 'base_link'
-                #  odom_msg.pose.pose.position.x = lat_global
-                #  odom_msg.pose.pose.position.y = lon_global
-                #  odom_msg.pose.pose.position.z = depth
-
-                #  odom_pub.publish(odom_msg)
-
-                time.sleep(1)
-
-        except KeyboardInterrupt:
-            pass
-    
-        print("Saving data to file: {}".format(filename))
-        f.write(gpx.to_xml())
-        f.close()
+            rospy.sleep(0.2)
 
 if __name__ == "__main__":
-    rospy.init_node('uw_gps_node')
+    rospy.init_node("uw_gps_node")
     try:
-        UWGps()
+        UWGPSNode()
     except rospy.ROSInterruptException:
         pass
