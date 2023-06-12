ccd3da0806af3074c09c75ec10fe08a79f0fe0a4
ignaciotb <ignaciotorroba@hotmail.com>
2023-06-12 16:05:33 +0200

    Merged with floatsam

--- floatsam_dead_reckoning/scripts/usv_dr_node.py
+++ floatsam_dead_reckoning/scripts/usv_dr_node.py
@@ -29,9 +29,9 @@ class VehicleDR(object):
         self.odom_frame = rospy.get_param('~odom_frame', 'sam/odom')
         self.map_frame = rospy.get_param('~map_frame', 'map')
         self.utm_frame = rospy.get_param('~utm_frame', 'utm')
-        # self.dvl_topic = rospy.get_param('~dvl_topic', '/sam/core/dvl')
-        # self.dvl_frame = rospy.get_param('~dvl_frame', 'dvl_link')
-        # self.dvl_period = rospy.get_param('~dvl_period', 0.2)
+        self.dvl_topic = rospy.get_param('~dvl_topic', '/sam/core/dvl')
+        self.dvl_frame = rospy.get_param('~dvl_frame', 'dvl_link')
+        self.dvl_period = rospy.get_param('~dvl_period', 0.2)
         self.dr_period = rospy.get_param('~dr_period', 0.02)
         self.rpm1_topic = rospy.get_param('~thrust1_fb', '/sam/core/rpm_fb1')
         self.rpm2_topic = rospy.get_param('~thrust2_fb', '/sam/core/rpm_fb2')
@@ -63,12 +63,12 @@ class VehicleDR(object):
 
         # # DVL integration
         self.pos_t = [0.] * 3
-        # self.t_dvl_prev = 0.
-        # self.dvl_on = False
-        # self.dvl_latest = DVL()
-        # self.dvl_latest.velocity.x = 0.
-        # self.dvl_latest.velocity.y = 0.
-        # self.dvl_latest.velocity.z = 0.
+        self.t_dvl_prev = 0.
+        self.dvl_on = False
+        self.dvl_latest = DVL()
+        self.dvl_latest.velocity.x = 0.
+        self.dvl_latest.velocity.y = 0.
+        self.dvl_latest.velocity.z = 0.
 
         # Depth measurements: init to zero
         self.b2p_trans = [0.] * 3
@@ -86,7 +86,7 @@ class VehicleDR(object):
         self.pub_odom = rospy.Publisher(self.odom_top, Odometry, queue_size=100)
         #self.sbg_sub = rospy.Subscriber(self.sbg_topic, SbgEkfQuat, self.sbg_cb)
         self.sbg_sub = rospy.Subscriber(self.sbg_topic, Imu, self.sbg_cb,  queue_size=10)
-        # self.dvl_sub = rospy.Subscriber(self.dvl_topic, DVL, self.dvl_cb)
+        self.dvl_sub = rospy.Subscriber(self.dvl_topic, DVL, self.dvl_cb)
         self.stim_sub = rospy.Subscriber(self.stim_topic, Imu, self.stim_cb, queue_size=10)
         # self.depth_sub = rospy.Subscriber(self.depth_top, PoseWithCovarianceStamped, self.depth_cb)
         self.gps_sub = rospy.Subscriber(self.gps_topic, Odometry, self.gps_cb)
@@ -152,14 +152,32 @@ class VehicleDR(object):
 
             pose_t = np.concatenate([self.pos_t, self.rot_t])    # Catch latest estimate from IMU
             rot_vel_t = self.vel_rot    # TODO: rn this keeps the last vels even if the IMU dies
-            
-            # Linear vels from diff drive
             lin_vel_t = np.zeros(3)
-            lin_vel_t[0:2] = np.matmul(np.matrix([[np.cos(pose_t[5]), 0], 
-                                                  [0, np.sin(pose_t[5])]]), 0.5 * self.KT * self.u)
+
+            # DVL data coming in
+            rot_mat_t = self.fullRotation(pose_t[3], pose_t[4], pose_t[5])
+            if self.dvl_on:
+
+                # Integrate linear velocities from DVL
+                # If last DVL msg isn't too old
+                if self.t_now - self.t_dvl_prev < self.dvl_period and \
+                        abs(self.dvl_latest.velocity.y) < 0.2 and \
+                        abs(self.dvl_latest.velocity.x) < 1.5 and \
+                        self.dvl_latest.velocity.x > -0.1:
+                    
+                    lin_vel_t = np.array([self.dvl_latest.velocity.x,
+                                            self.dvl_latest.velocity.y,
+                                            self.dvl_latest.velocity.z])    
+
+                    print("DVL vel ", lin_vel_t)
+
+            else:
+   
+                # Linear vels from diff drive
+                lin_vel_t[0:2] = np.matmul(np.matrix([[np.cos(pose_t[5]), 0], 
+                                                        [0, np.sin(pose_t[5])]]), 0.5 * self.KT * self.u)
             
             # Integrate linear vels                    
-            rot_mat_t = self.fullRotation(pose_t[3], pose_t[4], pose_t[5])
             step_t = np.matmul(rot_mat_t, lin_vel_t * self.dr_period)
             pose_t[0:2] += step_t[0:2]
 
@@ -274,6 +292,35 @@ class VehicleDR(object):
             # rospy.loginfo("Stim data coming in")
             self.t_stim_prev = imu_msg.header.stamp.to_sec()
             self.init_stim = True
+
+    def dvl_cb(self, dvl_msg):
+        # if self.rot_stim.any():
+
+        #     # # Project velocities into odom frame to correct for pitching of floatsam
+        #     euler = euler_from_quaternion(self.rot_stim)
+        #     mat_t = self.fullRotation(euler[0],euler[1],euler[2])
+
+        #     aux  = np.matmul(mat_t, np.array([dvl_msg.velocity.x,
+        #                                     dvl_msg.velocity.y,
+        #                                     dvl_msg.velocity.z]))
+        #     dvl_msg.velocity.x = aux[0]
+        #     dvl_msg.velocity.y = aux[1]
+        #     dvl_msg.velocity.z = aux[2]
+        self.dvl_latest = dvl_msg
+        
+        if self.dvl_on:
+            dt = dvl_msg.header.stamp.to_sec() - self.t_dvl_prev
+            self.t_dvl_prev = dvl_msg.header.stamp.to_sec()
+
+            # if dt > self.dvl_period:
+            #     print("Missed DVL meas")            
+
+        else:
+            rospy.loginfo("DVL data coming in")
+            self.t_dvl_prev = dvl_msg.header.stamp.to_sec()
+            self.t_now = dvl_msg.header.stamp.to_sec()
+            self.t_pub = dvl_msg.header.stamp.to_sec()
+            self.dvl_on = True
 
 
 if __name__ == "__main__":
